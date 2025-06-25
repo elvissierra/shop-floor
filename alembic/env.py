f2877b6d941ca0ab9de22b807b8c76ee050c6075
Elvis Sierra <elvissierra3@gmail.com>
2025-06-25 07:40:24 -0500

    connections settings local remote

--- alembic/env.py
+++ alembic/env.py
@@ -1,9 +1,10 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from sqlalchemy import engine_from_config, pool
 
 from alembic import context
+
+from sqlalchemy.ext.declarative import declarative_base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -24,7 +25,7 @@ target_metadata = None
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-
+    
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
